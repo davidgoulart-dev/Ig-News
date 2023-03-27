564e36407b8cfd9519564d6375adc1c05112122f
davidgoulart-dev <davidgoulart.tae@gmail.com>
2023-03-27 00:46:57 -0300

    Página Home criada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import styles from '../styles/home.module.scss'
+import { Header } from '@/components/Header'
+import styles from './home.module.scss'
 import { Roboto } from 'next/font/google'
 
 
@@ -8,10 +9,24 @@ const roboto = Roboto({
     })
 
 export default function Home() {
-  return (
-    <main className={roboto.className}>
+  return ( 
+  <>
+  <Header/>
+    <main className={styles.contentContainer}>
+     
+      <section className={styles.hero}>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the <span>React</span> world.</h1>
+        <p>
+          Get access to all the publications <br/>
+          <span>for $9.90 month</span>
+        </p>
+
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl coding"/ >
       
     </main>
-    
+    </>
   )
 }
