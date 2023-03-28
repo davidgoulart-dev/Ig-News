c8efbd189b51fcbaf053026a6b71bbf195198121
davidgoulart-dev <davidgoulart.tae@gmail.com>
2023-03-28 01:12:46 -0300

    att

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,61 @@
-import { Header } from '@/components/Header'
-import styles from './home.module.scss'
-import { Roboto } from 'next/font/google'
-import { SubscribeButton } from '@/components/SubscribeButton'
-import { getServerProduct } from '@/components/Product.server'
+import { Header } from '@/components/Header';
+import styles from './home.module.scss';
+import { Roboto } from 'next/font/google';
+import { SubscribeButton } from '@/components/SubscribeButton';
+import { getServerProduct } from '@/components/Product.server';
 
 const roboto = Roboto({
   subsets: ['latin'],
   weight: '400',
-})
+});
 
 interface Product {
-  priceId: string
-  amount: number
+  priceId: string;
+  amount: number;
 }
 
 interface HomeProps {
-  product: Product | null
+  product: Product | null;
 }
 
 export async function getServerSideProps() {
-  const product = await getServerProduct()
-  return { props: { product } }
+  const product = await getServerProduct();
+  return { props: { product } };
 }
 
-const Home: React.FC<HomeProps> = ({ product })=> {
+function formatCurrency(amount: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(amount);
+}
+
+const Home: React.FC<HomeProps> = ({ product }) => {
+  console.log('Home component rendered');
+  console.log('Product data:', product);
   return (
     <>
       <Header />
       <main className={styles.contentContainer}>
-        <section className={styles.hero}>
-          <span>👏 Hey, welcome</span>
-          <h1>News about the <span>React</span> world.</h1>
-          <p>
-            Get access to all the publications <br />
-            <span>for {product?.amount} month</span>
-           
-          </p>
-          <SubscribeButton />
-        </section>
+    <section className={styles.hero}>
+      <span>👏 Hey, welcome</span>
+      <h1>News about the <span>React</span> world.</h1>
+      <p>
+        Get access to all the publications <br />
+        <span>for {product && formatCurrency(product.amount)} per month</span>
+      </p>
+      <SubscribeButton />
+    </section>
 
-        <img src="/images/avatar.svg" alt="Girl coding" />
-      </main>
-    </>
-    
-  )
-}
+    <img src="/images/avatar.svg" alt="Girl coding" />
+  </main>
+</>
+);
+};
 
-export default Home
+export default Home;
+
+
+
+
+
--- src/components/Product.server.jsx
+++ src/components/Product.server.jsx
@@ -1,14 +1,13 @@
 // src/components/Product.server.jsx
 
+
 export async function getServerProduct() {
     try {
-      const response = await fetch('/api/pricing')
-      const data = await response.json()
-      return data.product
-      
+      const response = await fetch('http://localhost:3000/api/pricing');
+      const data = await response.json();
+      return data.product;
     } catch (error) {
-      console.error('Failed to fetch product data:', error)
-      return null
+      console.error('Failed to fetch product data:', error);
+      return null;
     }
   }
-  
